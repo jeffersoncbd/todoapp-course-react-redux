7a6cbe701d9c97a31585895f12d7f0be7c102015
Jefferson Carlos <jeff.carlosbd@gmail.com>
2020-01-29 16:53:47 +0000

    Integra redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,5 +3,15 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './redux/reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
